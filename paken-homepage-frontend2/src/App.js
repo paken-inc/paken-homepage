56c206c6f43fb977b6857158dc962ccc1edcd7d3
Tomás Antunes <tomasantunes@gmail.com>
2022-07-27 11:08:28 +0100

    Fix frontend folder

---
+++ paken-homepage-frontend2/src/App.js
@@ -0,0 +1,16 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+function App() {
+
+  useEffect(() => {
+
+  }, [])
+  
+  return (
+    <div className="container">
+      <h1>Paken</h1>
+    </div>
+  );
+}
+export default App;
