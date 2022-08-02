9015aceb8c85055559e8aaf837701d6f92e6709c
Tomás Antunes <tomasantunes@gmail.com>
2022-08-02 12:15:25 +0100

    Add keywords to front page

--- paken-homepage-frontend/src/components/Hero.js
+++ paken-homepage-frontend/src/components/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
       <div class="px-4 py-5 text-center bg-grey">
         <h1 class="display-5 fw-bold">Welcome to Paken</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">Web Development, Software, Mobile, Cloud, Data Science</p>
+          <p class="lead mb-4">Web Development, Software, Mobile, Cloud, Data Science, AI, Cybersecurity</p>
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <Link to="/sign-up" type="button" class="btn btn-primary btn-lg px-4 gap-3">Sign Up</Link>
           </div>
